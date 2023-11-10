3241d547292cf0bfcfef9ef7e0f8a4bc63035e7f
PhiferTristan <phifer.tristan@outlook.com>
2023-11-10 14:58:18 -0600

    setup for register and login pages along with a navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import ApplicationViews from "./components/ApplicationViews";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApplicationViews />
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+  return (
+    <>
+      <div className="text-white pl-10 pr-10">
+        <h1 className="text-4xl">Welcome to Readers Digest</h1>
+      </div>
+    </>
+  );
+};
